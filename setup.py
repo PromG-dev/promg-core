f26ae18545e3140f3dbdcb56ec107f3f22c550f3
Ava-S <ava.swevels@gmail.com>
2023-07-25 11:50:24 +0200

    Update set up file

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@ from setuptools import find_packages, setup
 
 # read the contents of the README file
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='promg',
-    packages=find_packages(include=['promg']),
-    version='0.1.1a',
+    version='0.1.0a1',
     description='Pyhton library to build Event Knowledge Graphs',
     author='A. Swevels, D.Fahland',
     python_requires='>=3.7',
     install_requires=['neo4j', 'numpy', 'pandas', 'tabulate', 'tqdm'],
-    license = 'GPL 3.0',
+    license='GPL 3.0',
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    packages=find_packages(
+        where='src',
+        include=['promg*']),
+    package_dir={"": "src"}
 )
