d8f13de9fa808497b04c78e1bda2ebd530d0f295
Ava-S <ava.swevels@gmail.com>
2023-09-01 15:36:36 +0200

    Move functionality to determine entity by semantic header inside module (instead of EventKnowledgeGraph.py)

--- src/promg/ekg_modules/ekg_analysis.py
+++ src/promg/ekg_modules/ekg_analysis.py
@@ -1,3 +1,4 @@
+from .. import SemanticHeader
 from ..database_managers.db_connection import DatabaseConnection
 from ..utilities.performance_handling import Performance
 from ..cypher_queries.analysis_ql import AnalysisQueryLibrary as analysis_ql
@@ -7,7 +8,22 @@ class EKGAnalysis:
     def __init__(self):
         self.connection = DatabaseConnection()
 
+    def create_df_process_model(self, entity_type: str):
+        """
+        Create a DF process model
+
+        Args:
+            entity_type: The type of the entity
+
+        Raises:
+            ValueError: when the entity has not been defined
+        """
+        entity = SemanticHeader().get_entity(entity_type)
+        if entity_type is None:
+            raise ValueError(f"{entity_type} is not defined in semantic header")
+        self._create_df_process_model(entity=entity)
+
     @Performance.track("entity")
-    def create_df_process_model(self, entity):
+    def _create_df_process_model(self, entity):
         self.connection.exec_query(analysis_ql.get_aggregate_df_relations_query,
                                    **{"entity": entity})
