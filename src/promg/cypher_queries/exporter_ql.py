fabbc6ac79eef7af6079725a91ae3bedde6f4e03
Ava-S <ava.swevels@gmail.com>
2023-09-19 17:48:47 +0200

    Create documentation for exporter

--- src/promg/cypher_queries/exporter_ql.py
+++ src/promg/cypher_queries/exporter_ql.py
@@ -5,16 +5,19 @@ from ..database_managers.db_connection import Query
 class ExporterQueryLibrary:
 
     @staticmethod
-    def get_event_log_query(entity: ConstructedNodes, additional_event_attributes) -> Query:
+    def get_event_log_query(entity: ConstructedNodes, additional_event_attributes, additional_entity_attributes) -> Query:
         query_str = '''
-                MATCH (e:Event) - [:CORR] -> (n:$node_label)
-                RETURN n.sysId as caseId, e.activity as activity, e.timestamp as timestamp $extra_attributes
-                ORDER BY n.ID, e.timestamp
+                MATCH (e:Event) - [:$corr_type] -> (n:$node_label)
+                RETURN n.sysId as caseId, e.activity as activity, e.timestamp as timestamp $extra_attributes $entity_attributes
+                ORDER BY n.sysId, e.timestamp
             '''
 
         attributes_query = ",".join(f"e.{attribute} as {attribute}" for attribute in additional_event_attributes)
+        entity_attributes_query = ",".join(f"n.{attribute} as {attribute}" for attribute in additional_entity_attributes)
         return Query(query_str=query_str,
                      template_string_parameters={
                          "node_label": entity.get_label_string(),
-                         "extra_attributes": f", {attributes_query}" if len(additional_event_attributes) > 0 else ""
+                         "corr_type": entity.get_corr_type_strings(),
+                         "extra_attributes": f", {attributes_query}" if len(additional_event_attributes) > 0 else "",
+                         "entity_attributes": f", {entity_attributes_query}" if len(additional_entity_attributes) > 0 else ""
                      })
--- src/promg/modules/exporter.py
+++ src/promg/modules/exporter.py
@@ -12,7 +12,8 @@ class Exporter:
     def __init__(self):
         self.connection = DatabaseConnection()
 
-    def get_event_log(self, entity_type, additional_event_attributes: Optional[List[str]] = None) -> List[
+    def get_event_log(self, entity_type, additional_event_attributes: Optional[List[str]] = None,
+                      additional_entity_attributes: Optional[List[str]] = None) -> List[
         Dict[str, Any]]:
         """
         Get an event log extracted from the EKG for a specific entity and return it
@@ -20,6 +21,8 @@ class Exporter:
         Args:
             entity_type: The type of the entity --> contract states that it exists
             additional_event_attributes: list of different attributes of event that should also be stored in the
+            event log
+            additional_entity_attributes: list of different attributes of entity that should also be stored in the
             event log
 
         Returns:
@@ -33,9 +36,11 @@ class Exporter:
         if entity_object is None:
             raise ValueError(f"Entity {entity_type} is not defined in the semantic header")
 
-        return self._get_event_log(entity_object, additional_event_attributes)
+        return self._get_event_log(entity_object, additional_event_attributes, additional_entity_attributes)
 
-    def _get_event_log(self, entity: ConstructedNodes, additional_event_attributes: Optional[List[str]] = None) -> \
+    def _get_event_log(self, entity: ConstructedNodes,
+                       additional_event_attributes: Optional[List[str]] = None,
+                       additional_entity_attributes: Optional[List[str]] = None) -> \
             Optional[List[Dict[str, Any]]]:
         """
         Get an event log extracted from the EKG for a specific entity and return it
@@ -44,29 +49,38 @@ class Exporter:
             entity: The entity for which we want to extract an event log
             additional_event_attributes: list of different attributes of event that should also be stored in the
             event log
+            additional_entity_attributes: list of different attributes of entity that should also be stored in the
+            event log
 
         Returns:
             A list of events with its attributes in the form of a dictionary
         """
-        if additional_event_attributes is None:
-            additional_event_attributes = []
+        additional_event_attributes = [] if additional_event_attributes is None else additional_event_attributes
+        additional_entity_attributes = [] if additional_entity_attributes is None else additional_entity_attributes
+
         return self.connection.exec_query(exporter_ql.get_event_log_query,
                                           **{
                                               "entity": entity,
-                                              "additional_event_attributes": additional_event_attributes
+                                              "additional_event_attributes": additional_event_attributes,
+                                              "additional_entity_attributes": additional_entity_attributes
                                           })
 
-    def save_event_log(self, entity_type: str, additional_event_attributes: Optional[List[str]] = None) -> None:
+    def save_event_log(self, entity_type: str,
+                       additional_event_attributes: Optional[List[str]] = None,
+                       additional_entity_attributes: Optional[List[str]] = None) -> None:
         """
         Create an event log extracted from the EKG from a specific entity and store it as a csv file
 
         Args:
             entity_type: The type of the entity
-             additional_event_attributes: list of different attributes of event that should also be stored in the
+            additional_event_attributes: list of different attributes of event that should also be stored in the
              event log
+            additional_entity_attributes: list of different attributes of entity that should also be stored in the
+            event log
+
         """
 
-        event_log = self.get_event_log(entity_type, additional_event_attributes)
+        event_log = self.get_event_log(entity_type, additional_event_attributes, additional_entity_attributes)
         df = pd.DataFrame(event_log)
 
         current_file_path = os.path.dirname(__file__)
