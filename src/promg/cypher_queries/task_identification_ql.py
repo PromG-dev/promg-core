effb1c3ec759e7be2e8017efdafd84ff9ff0350a
eklijn <e.klijn@tue.nl>
2024-03-28 09:11:01 +0100

    Refactor df_ti & df_ta query parameters

--- src/promg/cypher_queries/task_identification_ql.py
+++ src/promg/cypher_queries/task_identification_ql.py
@@ -25,7 +25,6 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_create_task_instances_query(resource):
-        print("get_create_task_instances_query")
         # language=sql
         query_str = '''
                 CALL apoc.periodic.iterate(
@@ -56,7 +55,6 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_split_ti_nodes_create_new_1_query():
-        print("get_split_ti_nodes_create_new_1_query")
         # language=sql
         query_str = '''
                 CALL apoc.periodic.iterate(
@@ -81,7 +79,6 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_split_ti_nodes_create_new_2_query():
-        print("get_split_ti_nodes_create_new_2_query")
         # language=sql
         query_str = '''
                 CALL apoc.periodic.iterate(
@@ -107,7 +104,6 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_split_ti_nodes_remove_old_query():
-        print("get_split_ti_nodes_remove_old_query")
         # language=sql
         query_str = '''
                 CALL apoc.periodic.iterate(
@@ -122,7 +118,6 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_remove_df_joint_query():
-        print("get_remove_df_joint_query")
         # language=sql
         query_str = '''
                 MATCH ()-[r:DF_JOINT]-()
@@ -133,7 +128,6 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_correlate_ti_to_entity_query(entity):
-        print("get_correlate_ti_to_entity_query")
         # language=sql
         query_str = '''
                 CALL apoc.periodic.iterate(
@@ -151,29 +145,28 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_lift_df_to_task_instances_query(entity):
-        print("get_lift_df_to_task_instances_query")
         # language=sql
         query_str = '''
                 CALL apoc.periodic.iterate(
-                "MATCH (n:$entity_node_label)
+                'MATCH (n:$entity_node_label)
                  MATCH (ti:TaskInstance)-[:CORR]->(n)
                  WITH n, ti AS nodes ORDER BY ti.start_time, ID(ti)
                  WITH n, COLLECT (nodes) as nodeList
                  UNWIND range(0, size(nodeList)-2) AS i
-                 RETURN n, nodeList[i] as ti_first, nodeList[i+1] as ti_second",
-                "WITH n, ti_first, ti_second
-                    MERGE (ti_first)-[df:DF_TI_$entity_node_label]->(ti_second)",
+                 RETURN n, nodeList[i] as ti_first, nodeList[i+1] as ti_second',
+                'WITH n, ti_first, ti_second
+                    MERGE (ti_first)-[df:$df_ti_entity {entityType: "$entity_node_label"}]->(ti_second)',
                 {batchSize:$batch_size})
                     '''
 
         return Query(query_str=query_str,
                      template_string_parameters={
-                         "entity_node_label": entity.type
+                         "entity_node_label": entity.type,
+                         "df_ti_entity": entity.get_df_ti_label()
                      })
 
     @staticmethod
     def get_aggregate_task_instances_query(property):
-        print("get_aggregate_task_instances_query")
         # language=sql
         query_str = '''
                 CALL apoc.periodic.iterate(
@@ -191,7 +184,6 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_link_task_instances_to_aggregations_query(property):
-        print("get_link_task_instances_to_aggregations_query")
         # language=sql
         query_str = '''
                 CALL apoc.periodic.iterate(
@@ -210,7 +202,6 @@ class TaskIdentifierLibrary:
 
     @staticmethod
     def get_lift_df_to_task_aggregations_query(property, entity):
-        print("get_lift_df_to_task_aggregations_query")
         # language=sql
         query_str = '''
                 MATCH (ta1:TaskAggregation)<-[:OBSERVED]-(ti1:TaskInstance)-[df:DF_TI_$entity_node_label]
@@ -218,11 +209,12 @@ class TaskIdentifierLibrary:
                     WHERE ta1.Type = '$property' AND ta2.Type = '$property'
                 MATCH (ti1)-[:CORR]->(n:$entity_node_label)<-[:CORR]-(ti2)
                 WITH ta1, count(df) AS df_freq, ta2
-                MERGE (ta1)-[rel2:DF_TA_$entity_node_label]->(ta2) ON CREATE SET rel2.count=df_freq
+                MERGE (ta1)-[rel2:$df_ta_entity {entityType: '$entity_node_label'}]->(ta2) ON CREATE SET rel2.count=df_freq
                 '''
 
         return Query(query_str=query_str,
                      template_string_parameters={
                          "property": property,
-                         "entity_node_label": entity.type
+                         "entity_node_label": entity.type,
+                         "df_ta_entity": entity.get_df_ta_label()
                      })
