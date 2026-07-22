0a55f7a18a54fc1f3dc9489f8da6820771c4355a
Ava-S <ava.swevels@gmail.com>
2026-07-22 09:52:43 +0200

    Add default timezone to config

--- src/promg/utilities/configuration.py
+++ src/promg/utilities/configuration.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 class Configuration:
     def __init__(self, semantic_header_path, dataset_description_path, db_name, uri, user, password,
-                 verbose, batch_size, use_sample, use_preprocessed_files, import_directory=None):
+                 verbose, batch_size, use_sample, use_preprocessed_files, timezone, import_directory=None):
         self.semantic_header_path = semantic_header_path
         self.dataset_description_path = dataset_description_path
         self.import_directory = import_directory
@@ -16,6 +16,10 @@ class Configuration:
         self.batch_size = batch_size
         self.use_sample = use_sample
         self.use_preprocessed_files = use_preprocessed_files
+        self.timezone = timezone
+
+    def get_timezone(self):
+        return self.timezone
 
     @staticmethod
     def init_conf_with_config_file(path=Path("config.yaml")):
@@ -35,6 +39,11 @@ class Configuration:
         _use_sample = config["use_sample"]
         _use_preprocessed_files = config["use_preprocessed_files"]
 
+        if "timezone" in config:
+            _timezone = config["timezone"]
+        else:
+            _timezone = None
+
         return Configuration(semantic_header_path=_semantic_header_path,
                              dataset_description_path=_dataset_description_path,
                              import_directory=_import_directory,
@@ -45,4 +54,5 @@ class Configuration:
                              verbose=_verbose,
                              batch_size=_batch_size,
                              use_sample=_use_sample,
-                             use_preprocessed_files=_use_preprocessed_files)
+                             use_preprocessed_files=_use_preprocessed_files,
+                             timezone=_timezone)
