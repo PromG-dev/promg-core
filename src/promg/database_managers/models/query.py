adea36acd378314d6e1815b595b7367a4314aae5
s156229 <ava.swevels@proton.me>
2026-07-27 11:24:36 +0200

    Fix type hinting

--- src/promg/database_managers/models/query.py
+++ src/promg/database_managers/models/query.py
@@ -1,10 +1,10 @@
 from string import Template
-from typing import Optional, Dict
+from typing import Optional, Dict, Any
 
 
 class Query:
-    def __init__(self, query_str: str, database: str = None, parameters: Optional[Dict[str, any]] = None,
-                 template_string_parameters: Optional[Dict[str, any]] = None):
+    def __init__(self, query_str: str, database: Optional[str] = None, parameters: Optional[Dict[str, Any]] = None,
+                 template_string_parameters: Optional[Dict[str, Any]] = None):
         if template_string_parameters is not None:
             self.query_string = Template(query_str).safe_substitute(template_string_parameters)
         else:
